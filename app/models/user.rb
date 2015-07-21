ac2935b7053d3b986dc010c5ff17c9cbaf3e4773
Patricio de la Guardia <pdelaguardia87@gmail.com>
2015-07-21 10:36:59 -0400

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255},
+                              uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+
+end
