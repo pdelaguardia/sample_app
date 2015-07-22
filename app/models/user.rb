a68850007733b2cf1c497d50c0165f084fd3e3f6
Patricio de la Guardia <pdelaguardia87@gmail.com>
2015-07-22 11:46:56 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255},
                               uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
-
 end
--- db/migrate/20150721142852_add_index_to_users_email.rb
+++ db/migrate/20150721142852_add_index_to_users_email.rb
@@ -1,6 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration
   def change
-    before_save {self.email = email.downcase}
     add_index :users, :email, unique: true
   end
 end
